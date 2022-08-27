2abe22893cf2c8776bcae88b8dd89b3fb66e59ce
t-tonyo-maru <atsuji@atsujinoMacBook-Pro.local>
2022-08-27 22:47:13 +0900

    add: input要素にイベントをセット

--- src/ts/modules/setForm/setForm.ts
+++ src/ts/modules/setForm/setForm.ts
@@ -8,25 +8,45 @@ export const setForm = ({
   inputUserIdEl: Element
   inputPasswordEl: Element
 }) => {
+  // 入力中か
+  let isFocusUserIdEl = false
+  let isFocusPasswordEl = false
   // バリデーション結果
   let isValidUserId = false
   let isValidPassword = false
 
   // ユーザーIDのイベントセット
+  // focus イベント
+  inputUserIdEl.addEventListener('focus', () => {
+    isFocusUserIdEl = true
+  })
+  // blur イベントセット
   inputUserIdEl.addEventListener('blur', (event) => {
+    // 非入力中状態に更新
+    isFocusUserIdEl = false
+    // バリデーション情報を更新
     const userId = (event.target as HTMLInputElement).value
     isValidUserId = userId.length > 0
   })
 
   // パスワードのイベントセット
+  // focus イベント
+  inputPasswordEl.addEventListener('focus', () => {
+    isFocusPasswordEl = true
+  })
+  // blur イベントセット
   inputPasswordEl.addEventListener('blur', (event) => {
+    // 非入力中状態に更新
+    isFocusPasswordEl = false
+    // バリデーション情報を更新
     const password = (event.target as HTMLInputElement).value
     isValidPassword = getIsValidPassword(password)
   })
 
   return {
-    getisValidUserId: () => isValidUserId,
-    getisValidPassword: () => isValidPassword
+    // 入力中かを取得
+    getIsFocusUserIdEl: () => isFocusUserIdEl,
+    getIsFocusPasswordEl: () => isFocusPasswordEl,
     // バリデーション情報を取得
     getIsValidUserId: () => isValidUserId,
     getIsValidPassword: () => isValidPassword
